583da144e4f173baac94947996f303c490ba6d63
Brian <36707327+brianshukwit@users.noreply.github.com>
2020-11-11 08:51:34 -0600

    Add files via upload

---
+++ WanShiTong_Library/query.py
@@ -0,0 +1,24 @@
+#query.py
+import mysql.connector
+from mysql.connector import Error
+import pandas as pd
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+
+def read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as err:
+        print(f"Error: '{err}'")
