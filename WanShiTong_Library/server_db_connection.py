7e8af7f906b246718c0a00e34f1267ef1a1d15c9
Brian <36707327+brianshukwit@users.noreply.github.com>
2020-11-11 10:03:09 -0600

    Update and rename create.py to server_db_connection.py

--- WanShiTong_Library/server_db_connection.py
+++ WanShiTong_Library/server_db_connection.py
@@ -1,7 +1,7 @@
-#create.py
 import mysql.connector
 from mysql.connector import Error
 import pandas as pd
+
 
 def create_server_connection(host_name, user_name, user_password):
     connection = None
@@ -16,15 +16,6 @@ def create_server_connection(host_name, user_name, user_password):
         print(f"Error: '{err}'")
 
     return connection
-
-
-def create_database(connection, query):
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        print("Database created successfully")
-    except Error as err:
-        print(f"Error: '{err}'")
 
 
 def create_db_connection(host_name, user_name, user_password, db_name):
